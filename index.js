34e39458bdfb0bcf4d861ff268aa7892dfa65f42
Sunitha Iyadurai <sunithaiyadurai@gmail.com>
2021-03-09 00:33:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//Construct a for loop that combines two strings together, alternating the characters from each source.  Print the result.
+
+//Example: If string = “1234” and otherString = “5678”, then the output will be “15263748”.
+
+//For now, make sure the two strings contain the same number of characters!
+
+let  = 'Rutabagas';
+let  = 'Chemistry';
